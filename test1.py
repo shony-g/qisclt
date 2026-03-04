a5d840bc1e1f6f324f6ba3ad8c2960c3f089897c
shony-g <shony.g@gmail.com>
2026-03-05 02:40:20 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,7 @@
+# a=int(input("Please enter the cost of the product:Rs"))
+# b=int(input("Please enter the selleing price of the product:Rs"))
+# profit=b-a
+# if profit<0:
+#     print("The product sell with loss and the loss is: Rs",profit*-1,"/-")
+# else:
+#     print("The profit is :Rs",profit,"/-")
